f40d81ed9010008157dac7ccdc78f69d836cc5cf
Justin Bimbrahw <justin.bimbrahw@gmail.com>
2019-09-26 09:48:20 -0400

    Update zigzag.cpp

--- zigzag.cpp
+++ zigzag.cpp
@@ -8,6 +8,7 @@ public:
             sAns+=(s[i]);
 
             while(i<len){
+                //travel down and up the zig to the same row
                 int num =(numRows-1)*2-2*row;
                 if(num==0)
                     num=(numRows-1)*2;
@@ -16,7 +17,7 @@ public:
                     break;
                 sAns+=(s[i]);
 
-
+                //now travel up and down the zag to the same row
                 num=2*row;
                 if(num==0)
                     num=(numRows-1)*2;
